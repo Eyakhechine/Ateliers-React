8c3a0d95656e42979ea75c526bf18dd2db89ec4a
unknown <eya_khechine@hotmail.com>
2023-02-15 19:06:31 +0100

    like&buy

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,42 +3,57 @@
 import React,{Component} from 'react';
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import products from "../products.json";
 
 
 
 
-export default class Product extends Component{
 
-constructor(props) {
-  super(props)
-    this.state = { ...props };
+export default class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props};
+  }
 
+  like = (e) => {
+    e.preventDefault();
+    this.setState((oldState) => ({
+      produit: { ...oldState.produit, like: oldState.produit.like + 1 },
+    }));
+  };
 
-}
-render (){
+  buy = (e) => {
+    e.preventDefault();
+    this.setState((oldState) => ({
+      produit: { ...oldState.produit, quantity: oldState.produit.quantity - 1 },
+    }));
+   // this.props.alertstate(true);
+  };
 
-
-return (
-  <Card style={{ width: "20em" }}>
-    <Card.Img
-      variant="top"
-      src={require("../assets/images/" + this.state.produit.img)}
-    />
-    <Card.Body>
-      <Card.Text> {this.state.produit.name}</Card.Text>
-      <Card.Text> {this.state.produit.price}</Card.Text>
-      <Card.Text>{this.state.produit.description}</Card.Text>
-      <Card.Text>{this.state.produit.quantity}</Card.Text>
-      <Card.Text>{this.state.produit.like}</Card.Text>
-      <Button variant="primary">Like</Button>
-      <Button variant="secondary">buy</Button>
-    </Card.Body>
-  </Card>
-);
-
-
-
-}
-
+  render() {
+    return (
+      <Card style={{ width: "20em" }}>
+        <Card.Img
+          variant="top"
+          src={require("../assets/images/" + this.state.produit.img)}
+        />
+        <Card.Body>
+          <Card.Text> {this.state.produit.name}</Card.Text>
+          <Card.Text> {this.state.produit.price}</Card.Text>
+          <Card.Text>{this.state.produit.description}</Card.Text>
+          <Card.Text>{this.state.produit.quantity}</Card.Text>
+          <Card.Text>{this.state.produit.like}</Card.Text>
+          <Button variant="primary" onClick={this.like}>
+            Like
+          </Button>
+         <Button
+                  variant="primary"
+                //  disabled={this.state.product.quantity}
+                  onClick={this.buy}
+                >
+                  Buy
+                </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
