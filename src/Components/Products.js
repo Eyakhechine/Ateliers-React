70476f40038d38d2c3f5c60cf89b172f890b96c4
unknown <eya_khechine@hotmail.com>
2023-02-15 19:24:54 +0100

    alert welcome

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,20 +1,37 @@
 import Product from "./Product";
 import React, { Component } from "react";
 import products from "../products.json";
+import { Alert, Col, Container, Row, Stack } from "react-bootstrap";
 
 export default class Products extends Component {
   constructor(props) {
-    super(props)
-  this.state = { ...props };
-   
+    super(props);
+    this.state = { ...props };
   }
+
+  alertstate = (alert) => {
+    this.setState({ alert });
+    setTimeout(() => {
+      this.setState({ alert: false });
+    }, 2000);
+  };
   render() {
     return (
-      <>
-        {products.map((product, index) => (
-          <Product produit={product} key={index}></Product>
-        ))}
-      </>
+      <Container>
+        {" "}
+        <Alert variant="success">
+          <Alert.Heading>Hey, Welcome to Our shop MYSTORE</Alert.Heading>
+          <p>
+            thank you for choossing out store ,we hope you enjoy your shopping
+            experience
+          </p>
+        </Alert>
+        <>
+          {products.map((product, index) => (
+            <Product produit={product} key={index}></Product>
+          ))}
+        </>
+      </Container>
     );
   }
 }
