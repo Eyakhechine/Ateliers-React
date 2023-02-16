efcc9f20d570e859d90074d82431557e8e350365
unknown <eya_khechine@hotmail.com>
2023-02-16 12:05:04 +0100

    atelier2

---
+++ src/Components/Product.js
@@ -0,0 +1,62 @@
+
+
+import React,{Component} from 'react';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Stack } from "react-bootstrap";
+
+
+
+
+
+export default class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props};
+  }
+
+  like = (e) => {
+    
+    e.preventDefault();
+    this.setState((oldState) => ({
+      produit: { ...oldState.produit, like: oldState.produit.like + 1 },
+    }));
+  };
+
+  buy = (e) => {
+    e.preventDefault();
+    this.setState((oldState) => ({
+      produit: { ...oldState.produit, quantity: oldState.produit.quantity - 1 },
+    }));
+   // this.props.alertstate(true);
+  };
+
+  render() {
+    return (
+      <Card style={{ width: "20em" }}>
+        <Card.Img
+          variant="top"
+          src={require("../assets/images/" + this.state.produit.img)}
+        />
+        <Card.Body>
+          <Card.Text> {this.state.produit.name}</Card.Text>
+          <Card.Text> {this.state.produit.price}</Card.Text>
+          <Card.Text>{this.state.produit.description}</Card.Text>
+          <Card.Text>{this.state.produit.quantity}</Card.Text>
+          <Card.Text>{this.state.produit.like}</Card.Text>
+     
+          <Button variant="primary" onClick={this.like}>
+            Like
+          </Button>
+         <Button
+                  variant="info"
+                disabled={!this.state.produit.quantity}
+                  onClick={this.buy}
+                >
+                  Buy
+                </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
